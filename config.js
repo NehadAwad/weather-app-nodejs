4952c6f8fd22e45b0a869cdfa1a542e28a1e3c3e
NehadAwad <nehadakash622@gmail.com>
2020-12-21 09:03:24 +0600

    Init commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "d388c689ac4dde05849735c5bd48b7a0"
+    }
+}
+
+module.exports = constants;
